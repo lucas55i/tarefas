0bce3a9632c0f588e222b71d19f95b3a1b20b5b0
Lucas S C. Jesus <lucas.devj@outlook.com>
2023-02-01 16:48:16 -0300

    Criação do Componente TaskDetail

---
+++ src/components/TaskDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from './Button.jsx';
+const TaskDetails = () => {
+    return (
+        <>
+            <div className="back-button-container">
+                <Button />
+            </div>
+            <div className="task-detail-contaier">
+                <p></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius repellendus consectetur commodi quas, dolorum illo dignissimos. Eligendi quae iste dicta, molestiae iure blanditiis cumque, fugiat qui ipsum sunt quam necessitatibus.</p>
+            </div>
+        </>
+    );
+}
+
+export default TaskDetails;
